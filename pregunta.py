90c65599e6e114d683a0cb04c30c329d062de7db
JeanCarloH <55773226+JeanCarloH@users.noreply.github.com>
2022-10-17 19:07:02 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -9,13 +9,33 @@ espacio entre palabra y palabra.
 
 
 """
-import pandas as pd
 
+import pandas as pd
+import numpy as np
+import math
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    df = pd.read_fwf(
+        "clusters_report.txt", widths = [8,10,20,100], header = None,
+        names = ["cluster","cantidad_de_palabras_clave","porcentaje_de_palabras_clave", "-"],
+        skip_blank_lines = False, converters = {"porcentaje_de_palabras_clave": 
+        lambda x: x.rstrip(" %").replace(",",".")}).drop([0,1,2,3], axis=0)
+
+    porcentaje = df["-"]
+    df = df[df["cluster"].notna()].drop("-", axis=1)
+    df = df.astype({ "cluster": int, "cantidad_de_palabras_clave": int, "porcentaje_de_palabras_clave": float})
+
+    porcentaje_total = []
+    text = ""
+    for lin in porcentaje:
+        if isinstance(lin, str): text += lin+" "
+        else:
+            text = ", ".join([" ".join(x.split()) for x in text.split(",")])
+            porcentaje_total.append(text.rstrip("."))
+            text = ""
+            continue
+
+    df["principales_palabras_clave"] = porcentaje_total
 
     return df
